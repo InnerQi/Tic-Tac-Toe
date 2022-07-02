b89f52a708775d2c147826251129b8afe8934f0b
Nolan S. Hernandez <nhernandez9@gatech.edu>
2022-07-01 22:10:32 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# Tic-Tac-Toe game
+
+# create function that sets up Tic-Tac-Toe board
+# create a function that allows player to select "X" or "O"
+#
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
